57a6b6ee1bdf2a4134614572623885ff2b9e3358
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2025-02-26 15:17:33 -0500

    wrapping up

--- embedding/deepwalk.py
+++ embedding/deepwalk.py
@@ -1,11 +1,2 @@
-from .library import Graph
-from testing_graphs import graph_test_one
-
-def deepwalk(graph, walks_per_node, walk_length, embedding_dimension, window_size):
-    graph = Graph(graph_test_one, embedding_dimension)
-    for _ in walks_per_node:
-        random_walks = graph.randomwalk(walk_length, "deepwalk")
-        for node, walk in random_walks.items():
-            graph.skipgram(walk, window_size)
-    
-    return graph.node_embeddings
+from embedding.embedding_graph import graph_embedding
+graph_embedding(40, 12, 200, 1.2, 2, "deepwalk")
