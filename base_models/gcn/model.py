5a3844761040bc2e9676517ddd3892c747c9c61a
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2025-03-04 23:39:56 -0500

    migrating

--- base_models/gcn/model.py
+++ base_models/gcn/model.py
@@ -3,18 +3,24 @@ from base_models.gcn.layers import GraphConvolution
 
 
 class ConvolutionalGraphNetwork(torch.nn.Module):
-    def __init__(self, input_dimension, hidden_dimension, n_clases, n_layers):
+    def __init__(self, input_dimension, hidden_dimension, n_clases, n_layers, dropout = None):
         super().__init__()
         self.input_dimension = input_dimension
         self.hidden_dimension = hidden_dimension
         self.outpu_dimension = n_clases
+
+        self.dropout = dropout if dropout else 0
+
         self.layers = torch.nn.ModuleList()
         for _ in range(n_layers):
             self.layers.append(GraphConvolution(self.hidden_dimension, self.hidden_dimension))
         self.gcn1 = GraphConvolution(self.input_dimension, self.hidden_dimension)
         self.gcn2 = GraphConvolution(self.hidden_dimension, self.outpu_dimension)
+        self.dropout_layer = torch.nn.Dropout(p = self.dropout)
 
     def forward(self, input, normalized_adjacency):
+
+        input = self.dropout_layer(input)
         support = self.gcn1(input, normalized_adjacency)
         for layer in self.layers:
             support = self.layer(support, normalized_adjacency)
